381a8b7b47f76eb4f64028f90e3840f8b64d7645
Hristo Kirilov <hristo.kirilov07@gmail.com>
2024-02-27 22:23:11 +0200

    File Added

---
+++ JS Syntax Fundamentals/month_printer.js
@@ -0,0 +1,8 @@
+function solve(number) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if (number >= 1 && number<= 12){
+         console.log(months[number-1])
+    }else{
+        console.log("Error!")
+    }
+}
